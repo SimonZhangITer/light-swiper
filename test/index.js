fef0821cb5c98600cd0d8f74a4802119fba66baf
zhangren <zhangren@meituan.com>
2019-02-21 17:44:49 +0800

    fix import

--- test/index.js
+++ test/index.js
@@ -1,21 +1,19 @@
-import Swiper from "../swiper";
-
-const options2 = {
+var options2 = {
   continuous: true
 };
 
-const options3 = {
+var options3 = {
   width: 310,
   offset: 28,
   continuous: true
 }
 
-const options4 = {
+var options4 = {
   width: 310,
   offset: 28,
   continuous: true,
   callback: function (index, el) {
-    const dots = document.querySelectorAll('.dot')
+    var dots = document.querySelectorAll('.dot')
     for(let i = 0;i < dots.length; i++) {
       let dot = dots[i].querySelector('span')
       if (i == index) {
@@ -25,7 +23,7 @@ const options4 = {
   }
 }
 
-const mySwiper1 = new Swiper(document.querySelector('#siwper-1'))
-const mySwiper2 = new Swiper(document.querySelector('#siwper-2'), options2)
-const mySwiper3 = new Swiper(document.querySelector('#siwper-3'), options3)
-const mySwiper4 = new Swiper(document.querySelector('#siwper-4'), options4)
+var mySwiper1 = new lightSwiper(document.querySelector('#siwper-1'))
+var mySwiper2 = new lightSwiper(document.querySelector('#siwper-2'), options2)
+var mySwiper3 = new lightSwiper(document.querySelector('#siwper-3'), options3)
+var mySwiper4 = new lightSwiper(document.querySelector('#siwper-4'), options4)
